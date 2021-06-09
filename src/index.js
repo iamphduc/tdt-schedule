bc4e4f2ebd40ba91aef6eef242d37b833a2fc1cc
Pham Hong Duc <iamphduc@gmail.com>
2021-06-10 01:44:09 +0700

    fix pop up to headless true

--- src/index.js
+++ src/index.js
@@ -18,11 +18,11 @@ app.get('/', (req, res) => {
     res.send('hello');
 });
 
-app.get('/test', async(req, res) => {
+app.get('/test', async function(req, res) {
     const browser = await puppeteer.launch({
         executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
         defaultViewport: {width: 1920, height: 1080},
-        headless: false,
+        //headless: false,
     });
 
     const page = await browser.newPage();
@@ -60,15 +60,40 @@ app.get('/test', async(req, res) => {
     // schedule
     console.time('Schedule page');
 
-    const [schedule] = await Promise.all([
-        new Promise((resolve) => page.once('popup', async p => { // popup need headless false to work
-          await p.waitForNavigation({ waitUntil: 'networkidle0' });
-          resolve(p);
-        })),
-        page.click('#sidebar > .sidebar-collapse > #side-menu > #daotao > .nav a[href="https://lichhoc-lichthi.tdtu.edu.vn/tkb2.aspx"]')
-    ]);
+    // this is use for pop up
 
-    console.timeEnd('Schedule page');
+    // const [schedule] = await Promise.all([
+    //     new Promise((resolve) => page.once('popup', async p => { // popup need headless false to work
+    //       await p.waitForNavigation({ waitUntil: 'networkidle0' });
+    //       resolve(p);
+    //     })),
+    //     page.click('#sidebar > .sidebar-collapse > #side-menu > #daotao > .nav a[href="https://lichhoc-lichthi.tdtu.edu.vn/tkb2.aspx"]')
+    // ]);
+
+    await page.click('#sidebar > .sidebar-collapse > #side-menu > #daotao > .nav a[href="https://lichhoc-lichthi.tdtu.edu.vn/tkb2.aspx"]')
+
+    const getNewPageWhenLoaded =  async () => {
+        return new Promise(x =>
+            browser.on('targetcreated', async target => {
+                if (target.type() === 'page') {
+                    const newPage = await target.page();
+                    const newPagePromise = new Promise(y =>
+                        newPage.once('domcontentloaded', () => y(newPage))
+                    );
+                    const isPageLoaded = await newPage.evaluate(
+                        () => document.readyState
+                    );
+                    return isPageLoaded.match('complete|interactive')
+                        ? x(newPage)
+                        : x(newPagePromise);
+                }
+            })
+        );
+    };
+
+
+    const newPagePromise = getNewPageWhenLoaded();
+    const schedule = await newPagePromise;
 
 
     // select semester
